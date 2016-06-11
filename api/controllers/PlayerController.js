d0b8f71526841e5f51070bcdd7e36569a5535af0
Carlos <roigcarlo@gmail.com>
2016-06-11 15:14:51 +0200

    Added player and loginform v1

---
+++ api/controllers/PlayerController.js
@@ -0,0 +1,26 @@
+/**
+ * TrackController
+ *
+ * @description :: Server-side logic for dealing with Tracks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+ var request = require('request')
+ var querystring = require('querystring')
+
+
+module.exports = {
+
+  /**
+   * `PlayerController.player()`
+	 *
+	 * Initiales the login request using Spotify credentials
+   */
+  player: function (req, res) {
+
+    const track = req.param('trackID')
+
+    return res.view('player/player.ejs', {trackID:track});
+  },
+
+};
