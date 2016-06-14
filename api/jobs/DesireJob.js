11b68263ededdc00cb695fc0f9a4c6cbd0639dab
roigcarlo <roigcarlo@gmail.com>
2016-06-14 17:09:27 +0200

    DesireJob fixes

--- api/jobs/DesireJob.js
+++ api/jobs/DesireJob.js
@@ -1,4 +1,5 @@
 var nodemailer = require('nodemailer');
+var request = require('request')
 
 module.exports = function(agenda) {
     var job = {
@@ -11,7 +12,7 @@ module.exports = function(agenda) {
         //disabled: false,
 
         // method can be 'every <interval>', 'schedule <when>' or now
-        frequency: 'every 5 minutes',
+        frequency: 'every 15 seconds',
 
         // Jobs options
         //options: {
@@ -27,58 +28,58 @@ module.exports = function(agenda) {
           console.log("Foo job executed...");
 
           User.findOne({released:0,questionWhere:{'<':Date.now()},sort:'questionWhere DESC'}).exec(function freeDesire(err, entryUser) {
-
-            // Resquest an authorization for our app
-            var authOptions = {
-              url: 'https://accounts.spotify.com/api/token',
-              form: {
-                grant_type: 'refresh_token',
-                refresh_token: entryUser.refreshToken,
-              },
-              headers: {
-                'Authorization': 'Basic ' + (new Buffer(SpotifyService.clientId + ':' + SpotifyService.clientSecret).toString('base64'))
-              },
-              json: true
-            };
-
-            request.get(authOptions, function(error, response, body) {
-              DesireService.getCurated(updated.homebound, updated.explorer, body.access_token, function(curatedTrack) {
-                sp3uri = curatedTrack.uri.split(':')[2]
-                User.update({id:entryUser.id},{released:1,accessToken:body.access_token,stage3song:sp3uri}).exec(function(err, updated){
-                  var options_track_feature = {
-                    url: 'https://api.spotify.com/v1/audio-features/'+updated.stage1song,
-                    headers: { 'Authorization': 'Bearer ' + entryUser.accessToken }, // This is a test, no bearer token. Use it with moderation
-                    json: true
-                  };
-
-                  request.get(options_track_feature, function(error, response, body_track) {
-                    DesireService.sendDatagram(updated.homebound, updated.explorer, body_track, 3, 1, sp3uri)
-
-                    // create reusable transporter object using the default SMTP transport
-                    var poolConfig = {
-                        host: MailDataService.host,
-                        port: MailDataService.port,
-                        secure: true, // use SSL
-                        auth: {
-                            user: MailDataService.user,
-                            pass: MailDataService.pswd,
-                        }
+            if(entryUser != undefined) {
+                    // Resquest an authorization for our app
+                    var authOptions = {
+                      url: 'https://accounts.spotify.com/api/token',
+                      form: {
+                        grant_type: 'refresh_token',
+                        refresh_token: entryUser.refreshToken,
+                      },
+                      headers: {
+                        'Authorization': 'Basic ' + (new Buffer(SpotifyService.clientId + ':' + SpotifyService.clientSecret).toString('base64'))
+                      },
+                      json: true
                     };
-                    var transporter = nodemailer.createTransport(poolConfig);
 
-                    // setup e-mail data with unicode symbols
-                    var mailOptions = {
-                        from: 'Domestic <hello@DomesticTimeKeeper.com>', // sender address
-                        to: updated.mail, // list of receivers
-                        subject: 'YourDesire', // Subject line
-                        html: 'Test' // html body
-                    };
-                  })
-                })
-              })
+                    request.get(authOptions, function(error, response, body) {
+                      DesireService.getCurated(entryUser.homebound, entryUser.explorer, body.access_token, function(curatedTrack) {
+                        sp3uri = curatedTrack.uri.split(':')[2]
+                        User.update({id:entryUser.id},{released:1,accessToken:body.access_token,stage3song:sp3uri}).exec(function(err, updated){
+                          var options_track_feature = {
+                            url: 'https://api.spotify.com/v1/audio-features/'+updated.stage1song,
+                            headers: { 'Authorization': 'Bearer ' + entryUser.accessToken }, // This is a test, no bearer token. Use it with moderation
+                            json: true
+                          };
+
+                          request.get(options_track_feature, function(error, response, body_track) {
+                            DesireService.sendDatagram(updated.homebound, updated.explorer, body_track, 3, 1, sp3uri)
+
+                            // create reusable transporter object using the default SMTP transport
+                            var poolConfig = {
+                                host: MailDataService.host,
+                                port: MailDataService.port,
+                                secure: true, // use SSL
+                                auth: {
+                                    user: MailDataService.user,
+                                    pass: MailDataService.pswd,
+                                }
+                            };
+                            var transporter = nodemailer.createTransport(poolConfig);
+
+                            // setup e-mail data with unicode symbols
+                            var mailOptions = {
+                                from: 'Domestic <hello@DomesticTimeKeeper.com>', // sender address
+                                to: updated.mail, // list of receivers
+                                subject: 'YourDesire', // Subject line
+                                html: 'Test' // html body
+                            };
+                          })
+                        })
+                      })
+                    })
             }
-
-          }
+          })
 
           done();
         },
