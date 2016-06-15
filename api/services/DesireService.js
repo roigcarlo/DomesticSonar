2828277c9a4b87339834b8c79905e08aada4ac96
Carlos <roigcarlo@gmail.com>
2016-06-15 22:49:36 +0200

    More resilencd

--- api/services/DesireService.js
+++ api/services/DesireService.js
@@ -28,89 +28,113 @@ module.exports = {
 
     // Use the access token to access the Spotify Web API
     request.get(profileOptions, function(error, response, profilebody) {
-      console.log('Requesting profile')
 
-      // Homebound - Conserver
-      if(hvt && cve) {
-        // Most listened song of the last month
-        console.log('Fetching topTracks data')
-        var options_short = {
-          url: 'https://api.spotify.com/v1/me/top/tracks?limit='+1+'&time_range=medium_term',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
+      if(!error && response.statusCode === 200) {
 
-        request.get(options_short, function(error, response, body_track) {
-          console.log('DesireService',error,body_track)
-          if(body_track) {
-            if(body_track.items && body_track.items[0]){
-              console.log(callback)
-              callback(body_track.items[0])
-            }
-          }
-          // return body_track.items[0]
-        });
-      }
-
-      // Homebound - Explorer
-      if(hvt && !cve) {
-        // 1. Most listened track
-        // 2. get artists
-        // 3. get similar artist
-        // 4. get it's top track
-
-        // 1 + 2.
-        var options_track = {
-          url: 'https://api.spotify.com/v1/me/top/tracks?limit='+1+'&time_range=medium_term',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-
-        request.get(options_track, function(error, response, body_track) {
-
-          // 3.
-          var options_s_artist = {
-            url: 'https://api.spotify.com/v1/artists/'+body_track.items[0].artists[0].id+'/related-artists',
+        // Homebound - Conserver
+        if(hvt && cve) {
+          // Most listened song of the last month
+          console.log('Fetching topTracks data')
+          var options_short = {
+            url: 'https://api.spotify.com/v1/me/top/tracks?limit='+1+'&time_range=medium_term',
+            headers: { 'Authorization': 'Bearer ' + access_token },
             json: true
           };
 
-          request.get(options_s_artist, function(error, response, body_s_artist) {
-            // 4.
-            var options_s_track = {
-              url: 'https://api.spotify.com/v1/artists/'+body_s_artist.artists[0].id+'/top-tracks?country='+profilebody.country,
+          request.get(options_short, function(error, response, body_track) {
+            console.log('DesireService',error,body_track)
+            if(body_track) {
+              if(body_track.items && body_track.items[0]){
+                console.log(callback)
+                callback(body_track.items[0])
+              }
+            }
+            // return body_track.items[0]
+          });
+        }
+
+        // Homebound - Explorer
+        if(hvt && !cve) {
+          // 1. Most listened track
+          // 2. get artists
+          // 3. get similar artist
+          // 4. get it's top track
+
+          // 1 + 2.
+          var options_track = {
+            url: 'https://api.spotify.com/v1/me/top/tracks?limit='+1+'&time_range=medium_term',
+            headers: { 'Authorization': 'Bearer ' + access_token },
+            json: true
+          };
+
+          request.get(options_track, function(error, response, body_track) {
+
+            // 3.
+            var options_s_artist = {
+              url: 'https://api.spotify.com/v1/artists/'+body_track.items[0].artists[0].id+'/related-artists',
               json: true
             };
 
-            request.get(options_s_track, function(error, response, body_s_track) {
-              callback(body_s_track.tracks[0])
-              // return body_s_track.tracks[0]
+            request.get(options_s_artist, function(error, response, body_s_artist) {
+              // 4.
+              var options_s_track = {
+                url: 'https://api.spotify.com/v1/artists/'+body_s_artist.artists[0].id+'/top-tracks?country='+profilebody.country,
+                json: true
+              };
+
+              request.get(options_s_track, function(error, response, body_s_track) {
+                callback(body_s_track.tracks[0])
+                // return body_s_track.tracks[0]
+              });
             });
           });
-        });
-      }
+        }
 
-      // Tastemaker - Conserver
-      if(!hvt && cve) {
-        // Song from discovery week
-        var options_playlist = {
-          url: 'https://api.spotify.com/v1/users/'+profilebody.id+'/playlists',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
+        // Tastemaker - Conserver
+        if(!hvt && cve) {
+          // Song from discovery week
+          var options_playlist = {
+            url: 'https://api.spotify.com/v1/users/'+profilebody.id+'/playlists',
+            headers: { 'Authorization': 'Bearer ' + access_token },
+            json: true
+          };
 
-        request.get(options_playlist, function(error, response, body_playlist) {
+          request.get(options_playlist, function(error, response, body_playlist) {
 
-          var discovery
-          for( var pl in body_playlist.items) {
-            if(body_playlist.items[pl].uri.split(':')[2] == 'spotifydiscover') {
-              discovery = body_playlist.items[pl].id
+            var discovery
+            for( var pl in body_playlist.items) {
+              if(body_playlist.items[pl].uri.split(':')[2] == 'spotifydiscover') {
+                discovery = body_playlist.items[pl].id
+              }
             }
-          }
 
-          console.log(discovery)
+            console.log(discovery)
+
+            var options_track = {
+              url: 'https://api.spotify.com/v1/users/spotifydiscover/playlists/'+discovery+'/tracks?limit=1',
+              headers: { 'Authorization': 'Bearer ' + access_token },
+              json: true
+            };
+
+            request.get(options_track, function(error, response, body_track) {
+              callback(body_track.items[0].track)
+              // return body_track.items[0].track
+            });
+          });
+
+        }
+
+        // Tastemaker - Explorer
+        if(!hvt && !cve) {
+          // Song from discovery week
+          var options_playlist = {
+            url: 'https://api.spotify.com/v1/users/spotify/playlists',
+            headers: { 'Authorization': 'Bearer ' + access_token },
+            json: true
+          };
 
           var options_track = {
-            url: 'https://api.spotify.com/v1/users/spotifydiscover/playlists/'+discovery+'/tracks?limit=1',
+            url: 'https://api.spotify.com/v1/users/spotify/playlists/3rgsDhGHZxZ9sB9DQWQfuf/tracks?limit=1',
             headers: { 'Authorization': 'Bearer ' + access_token },
             json: true
           };
@@ -119,30 +143,9 @@ module.exports = {
             callback(body_track.items[0].track)
             // return body_track.items[0].track
           });
-        });
-
-      }
-
-      // Tastemaker - Explorer
-      if(!hvt && !cve) {
-        // Song from discovery week
-        var options_playlist = {
-          url: 'https://api.spotify.com/v1/users/spotify/playlists',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-
-        var options_track = {
-          url: 'https://api.spotify.com/v1/users/spotify/playlists/3rgsDhGHZxZ9sB9DQWQfuf/tracks?limit=1',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-
-        request.get(options_track, function(error, response, body_track) {
-          callback(body_track.items[0].track)
-          // return body_track.items[0].track
-        });
-
+        }
+      } else {
+        console.log('Unable to calculate user curated')
       }
     })
   },
