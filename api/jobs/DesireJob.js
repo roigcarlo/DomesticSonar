444b810dc13cc623f8f35def69f8c0bb7cc31fa1
Carlos <roigcarlo@gmail.com>
2016-06-15 23:57:51 +0200

    Tentatively added images to the mail

--- api/jobs/DesireJob.js
+++ api/jobs/DesireJob.js
@@ -2,7 +2,7 @@ var nodemailer = require('nodemailer')
 var request = require('request')
 var querystring = require('querystring')
 
-var mailtemplate = '<div style="width: 500px; background-color: #3f3f3f; padding:40px 0px 20px 0px; text-align: center; color: white; font-family: Arial, Helvetica, sans-serif";>Hi @NAME, here\'s your<br><span style="font-weight: bold">Song</span> for <span style="text-decoration: underline">The Timekeeper</span><div style="width: 250px; background-color: #7ed548; margin:40px 115px 0px 115px; color: white; border-radius: 0.25em; padding: 10px"><br><div style="width:77px ; height: 94px"></div><br><span style="font-size: 24px; font-weight: bold">This is your <br>moment, <br>and this <br>one your <br>song: <br></span><br>@SONG<br><br><br><a style="background-color: #eaeaea; color: black; border-radius: 0.25em; padding: 10px; text-decoration: none;" href="">LISTEN YOUR SONG</a><br><br><br>a project of<br>@DDSLOGO<br><br><br>Proudly co-produced with:<br>@SONARLOGO<br><br><br>In strong collaboration with:<br>@SPOTYLOGO<br><br></div><div style="margin-top: 30px; margin-bottom: 20px; color: #7ed548"><a href="http://domesticstreamers.com/" style="text-decoration: none; color: #7ed548">Check out more experiments</a></div></div>'
+var mailtemplate = '<div style="width: 500px; background-color: #3f3f3f; padding:40px 0px 20px 0px; text-align: center; color: white; font-family: Arial, Helvetica, sans-serif";>Hi @NAME, here\'s your<br><span style="font-weight: bold">Song</span> for <span style="text-decoration: underline">The Timekeeper</span><div style="width: 250px; background-color: #7ed548; margin:40px 115px 0px 115px; color: white; border-radius: 0.25em; padding: 10px"><br>@BOMB<br><span style="font-size: 24px; font-weight: bold">This is your <br>moment, <br>and this <br>one your <br>song: <br></span><br>@SONG<br><br><br><a style="background-color: #eaeaea; color: black; border-radius: 0.25em; padding: 10px; text-decoration: none;" href="">LISTEN YOUR SONG</a><br><br><br>a project of<br>@DDSLOGO<br><br><br>Proudly co-produced with:<br>@SONARLOGO<br><br><br>In strong collaboration with:<br>@SPOTYLOGO<br><br></div><div style="margin-top: 30px; margin-bottom: 20px; color: #7ed548"><a href="http://domesticstreamers.com/" style="text-decoration: none; color: #7ed548">Check out more experiments</a></div></div>'
 
 module.exports = function(agenda) {
     var job = {
@@ -33,8 +33,6 @@ module.exports = function(agenda) {
 
           User.findOne({released:0,questionWhere:{'<':Date.now()},sort:'questionWhere DESC'}).exec(function freeDesire(err, entryUser) {
 
-            // console.log(entryUser)
-
             if(entryUser != undefined) {
 
                     // Resquest an authorization for our app
@@ -53,31 +51,25 @@ module.exports = function(agenda) {
 
                     request.post(authOptions, function(error, response, body) {
 
-                      // console.log('refreshTokenRequest', body.acces_token, entryUser.accessToken)
-
                       DesireService.getCurated(entryUser.homebound > 60, entryUser.explorer < 50, body.access_token, function(curatedTrack) {
-
-                        // console.log('curated song', curatedTrack)
 
                         sp3uri = curatedTrack.uri.split(':')[2]
                         User.update({id:entryUser.id},{released:0,accessToken:body.access_token,stage3song:sp3uri}).exec(function(err, updated){
 
-                          //console.log(err,updated)
-
                           mailtemplate = mailtemplate.replace('@NAME',entryUser.nick)
                           mailtemplate = mailtemplate.replace('@SONG',curatedTrack.name )
 
-                          mailtemplate = mailtemplate.replace('@DDSLOGO',curatedTrack.name )
-                          mailtemplate = mailtemplate.replace('@SONARLOGO',curatedTrack.name )
-                          mailtemplate = mailtemplate.replace('@SPOTYLOGO',curatedTrack.name )
+                          mailtemplate = mailtemplate.replace('@BOMB','<img src="cid:unique@bomb.ee">')
+
+                          mailtemplate = mailtemplate.replace('@DDSLOGO','<img src="cid:unique@ddslogo.ee">' )
+                          mailtemplate = mailtemplate.replace('@SONARLOGO','<img src="cid:unique@sonarlogo.ee">' )
+                          mailtemplate = mailtemplate.replace('@SPOTYLOGO','<img src="cid:unique@spotilogo.ee">' )
 
                           var options_track_feature = {
                             url: 'https://api.spotify.com/v1/audio-features/'+updated[0].stage1song,
                             headers: { 'Authorization': 'Bearer ' + body.access_token },
                             json: true
                           };
-
-                          // console.log('========>','https://api.spotify.com/v1/audio-features/'+updated[0].stage1song)
 
                           request.get(options_track_feature, function(error, response, body_track) {
                             DesireService.sendDatagram(updated[0].id, updated[0].nick, updated[0].homebound, updated[0].explorer, body_track, 3, 1, sp3uri, curatedTrack.name, curatedTrack.artists[0].name)
@@ -101,13 +93,29 @@ module.exports = function(agenda) {
                                 from: 'TimeKeeper <sonar@domesticstreamers.com>', // sender address
                                 to: updated[0].mail, // list of receivers
                                 subject: 'YourDesire', // Subject line
-                                html: '<span>'+JSON.stringify(updated[0])+'</span>' // html body
-                                html: 'Embedded image: <img src="cid:unique@kreata.ee"/>',
-                                attachments: [{
-                                    filename: 'image.png',
+                                html: mailtemplate
+                                attachments: [
+                                  {
+                                    filename: 'bomb.png',
                                     path: MailDataService.imgp,
-                                    cid: 'unique@kreata.ee' //same cid value as in the html img src
-                                }]
+                                    cid: 'unique@bomb.ee' //same cid value as in the html img src
+                                  },
+                                  {
+                                    filename: 'ddslogo.png',
+                                    path: MailDataService.imgp,
+                                    cid: 'unique@ddslogo.ee' //same cid value as in the html img src
+                                  },
+                                  {
+                                    filename: 'sonarlogo.png',
+                                    path: MailDataService.imgp,
+                                    cid: 'unique@sonarlogo.ee' //same cid value as in the html img src
+                                  },
+                                  {
+                                    filename: 'spotilogo.png',
+                                    path: MailDataService.imgp,
+                                    cid: 'unique@spotilogo.ee' //same cid value as in the html img src
+                                  }
+                              ]
                             };
 
                             // send mail with defined transport object
