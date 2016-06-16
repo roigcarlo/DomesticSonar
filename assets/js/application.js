fbed723bc97acbca247c95036fbd5df18d98df3d
roigcarlo <roigcarlo@gmail.com>
2016-06-16 12:36:18 +0200

    Unix time in ms

--- assets/js/application.js
+++ assets/js/application.js
@@ -198,17 +198,17 @@ function lockSlide(prev, actv, next, swiper, onSlide) {
 }
 
 Date.prototype.addHours = function(h) {
-   this.setTime(this.getTime() + (h*60*60));
+   this.setTime(this.getTime() + (h*60*60*1000));
    return this;
 }
 
 Date.prototype.addDays = function(d) {
-   this.setTime(this.getTime() + (d*24*60*60));
+   this.setTime(this.getTime() + (d*24*60*60*1000));
    return this;
 }
 
 Date.prototype.addMonths = function(m) {
-   this.setTime(this.getTime() + (m*30*24*60*60));
+   this.setTime(this.getTime() + (m*30*24*60*60*1000));
    return this;
 }
 
